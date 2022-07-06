f3d9884c64cbc9ee1f42c4a63eba32d5e33cf0bc
jiuer12138 <18815283494@163.com>
2022-07-06 20:42:38 +0800

    01作业tab栏切换注册完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="demo">
+    <h2>
+      <span :class="{ active: isTrue }" @click="ChangePhone">手机注册</span> |
+      <span :class="{ active: !isTrue }" @click="ChangeEmail">邮箱注册</span>
+    </h2>
+    <keep-alive>
+      <component :is="comName">可自定义</component>
+    </keep-alive>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PhoneRegister from './components/PhoneRegister.vue'
+import EmailRegister from './components/EmailRegister.vue'
 export default {
-  name: 'App',
+  data() {
+    return {
+      isTrue: true,
+      comName: 'PhoneRegister',
+    }
+  },
   components: {
-    HelloWorld
-  }
+    EmailRegister,
+    PhoneRegister,
+  },
+  mounted() {},
+  computed: {},
+  methods: {
+    ChangePhone() {
+      this.isTrue = true
+      this.comName = 'PhoneRegister'
+    },
+    ChangeEmail() {
+      this.isTrue = false
+      this.comName = 'EmailRegister'
+    },
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.demo {
+  position: absolute;
+  width: 500px;
+  left: calc(50% - 250px);
+  top: 10%;
+  padding: 10px;
+  background: rgba(255, 255, 255, 0.7);
+  backdrop-filter: blur(6px);
+  border-radius: 5px;
+}
+h2 {
+  cursor: pointer;
+}
+.active {
+  color: #409eff;
+  font-size: 30px;
 }
 </style>
