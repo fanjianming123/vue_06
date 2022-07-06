aea5a52bd618fd10e8baf1e9929d004d40afb8ef
jiuer12138 <18815283494@163.com>
2022-07-06 22:17:18 +0800

    02作业表格渲染完成

---
+++ src/components/TableComponent.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+      <table border="1">
+          <thead>
+              <tr>
+                  <th>序号</th>
+                  <th>姓名</th>
+                  <th>年龄</th>
+                  <th>头像</th>
+              </tr>
+          </thead>
+          <tbody>
+              <TableItem v-for="item in list " :key='item.id' :item='item'>
+                <template v-slot:ItemId='scope'>
+                    {{scope.row}}
+                </template>
+                <template v-slot:ItemName='scope'>
+                    {{scope.row}}
+                </template>
+                <template v-slot:ItemAge='scope'>
+                    {{scope.row}}
+                </template>
+                <template v-slot:ItemUrl='scope' >
+                    <a :href="scope.row" v-if="type==0">{{scope.row}}</a>
+                    <img :src="scope.row" v-else-if="type==1">
+                    <div v-else>{{scope.row}}</div>
+                </template>
+              </TableItem>
+          </tbody>
+      </table>
+  </div>
+</template>
+
+<script>
+import TableItem from './TableItem.vue'
+export default {
+data() {
+    return {
+      type:0,
+        list: [
+        {
+          id: 100,
+          name: "小传同学",
+          age: 18,
+          headImgUrl:
+            "http://yun.itheima.com/Upload/./Images/20210303/603f2d2153241.jpg",
+          type: 0, // 0 显示成a标签
+        },
+        {
+          id: 101,
+          name: "小黑同学",
+          age: 25,
+          headImgUrl:
+            "http://yun.itheima.com/Upload/./Images/20210304/6040b101a18ef.jpg",
+          type: 1, // 1 显示成图片
+        },
+        {
+          id: 102,
+          name: "智慧同学",
+          age: 21,
+          headImgUrl:
+            "http://yun.itheima.com/Upload/./Images/20210302/603e0142e535f.jpg",
+          type: 1, // 1 显示成图片
+        },
+        {
+          id: 103,
+          name: "智慧同学",
+          age: 21,
+          headImgUrl:
+            "http://yun.itheima.com/Upload/./Images/20210302/603e0142e535f.jpg",
+          type: 2, // 2 显示成文本
+        },
+      ],
+    }
+},
+components:{
+    TableItem
+}
+}
+</script>
---
+++ src/components/TableItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <tr>
+    <td><slot name="ItemId"   :row='item.id'></slot></td>
+    <td><slot name="ItemName" :row='item.name'>{{item.name}}</slot></td>
+    <td><slot name="ItemAge"  :row='item.age'>{{item.age}}</slot></td>
+    <td><slot name="ItemUrl"  :row='item.headImgUrl'></slot></td>
+  </tr>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+  props:{
+   item:{
+    type:Object,
+    default:()=>({})
+   }
+  },
+  components: {},
+  mounted() {},
+  computed: {},
+  methods: {},
+}
+</script>
+<style scoped></style>
